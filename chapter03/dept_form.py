f0a76a63cddfbcd9d1c279d5ade374e31468d0ec
Builder <kgdfather@gmail.com>
2019-12-12 16:42:38 +0900

    --chapter03-- tbl_widget 우클릭시 '수정(QAction)' 기능 추가

--- chapter03/dept_form.py
+++ chapter03/dept_form.py
@@ -21,7 +21,7 @@ class DepartUI(QWidget):
 
         # signal & slot
         self.ui.btn_add.clicked.connect(self.add_item)
-        self.ui.btn_update.clicked.connect(self.update_item)
+        # self.ui.btn_update.clicked.connect(self.update_item)
         self.ui.btn_del.clicked.connect(self.delete_item)
         self.ui.btn_init.clicked.connect(self.init_item)
 
@@ -31,9 +31,11 @@ class DepartUI(QWidget):
         data = [(1, "마케팅", 8), (2, "개발", 10), (3, "인사", 20)]
         self.load_data(data)
 
+    # enumerate(): 인덱스 번호와 컬렉션의 원소를 tuple형태로 반환한다.
     def load_data(self, data):
         for idx, (no, name, floor) in enumerate(data):
             item_no, item_name, item_floor = self.create_item(no, name, floor)
+            print(idx, item_no, item_name, item_floor)
             nextIdx = self.tbl_widget.rowCount()
             self.tbl_widget.insertRow(nextIdx)
             self.tbl_widget.setItem(nextIdx, 0, item_no)
@@ -43,14 +45,24 @@ class DepartUI(QWidget):
     def set_context_menu(self, tv):
         tv.setContextMenuPolicy(Qt.ActionsContextMenu)
         update_action = QAction("수정", tv)
-        delete_action = QAction("삭제", tv)
+        delete_action = QAction("삭제(미구현)", tv)
+        # todo tbl_widget 우클릭시 삭제 QAction 기능 추가하기
         tv.addAction(update_action)
         tv.addAction(delete_action)
         update_action.triggered.connect(self.__update)
         delete_action.triggered.connect(self.__delete)
 
     def __update(self):
-        QMessageBox.information(self, 'Update', "확인", QMessageBox.Ok)
+        # QMessageBox.information(self, 'Update', "확인", QMessageBox.Ok)
+        selectedItems = self.tbl_widget.selectedItems()
+        self.ui.le_no.setText(selectedItems[0].text())
+        self.ui.le_name.setText(selectedItems[1].text())
+        self.ui.le_floor.setText(selectedItems[2].text())
+        self.ui.btn_add.setText("수정")
+        self.ui.btn_add.clicked.disconnect()
+        self.ui.btn_add.clicked.connect(self.update_item)
+
+        self.tbl_widget.setSelectionMode(QAbstractItemView.NoSelection)
 
     def __delete(self):
         QMessageBox.information(self, 'Delete', "확인", QMessageBox.Ok)
@@ -58,7 +70,6 @@ class DepartUI(QWidget):
     def add_item(self):
         item_no, item_name, item_floor = self.get_item_from_le()
         currentIdx = self.tbl_widget.rowCount()
-        print(type(currentIdx))
         self.tbl_widget.insertRow(currentIdx)
         self.tbl_widget.setItem(currentIdx, 0, item_no)
         self.tbl_widget.setItem(currentIdx, 1, item_name)
@@ -87,13 +98,25 @@ class DepartUI(QWidget):
         return item_no, item_name, item_floor
 
     def update_item(self):
-        pass
+        selectedIndexes = self.tbl_widget.selectedIndexes()
+        item_no, item_name, item_floor = self.get_item_from_le()
+
+        self.tbl_widget.setItem(selectedIndexes[0].row(), 0, item_no)
+        self.tbl_widget.setItem(selectedIndexes[1].row(), 1, item_name)
+        self.tbl_widget.setItem(selectedIndexes[2].row(), 2, item_floor)
+
+        self.ui.btn_add.setText("추가")
+        self.ui.btn_add.clicked.disconnect()
+        self.ui.btn_add.clicked.connect(self.add_item)
+
+        self.tbl_widget.setSelectionMode(QAbstractItemView.SingleSelection)
+
+        self.init_item()
 
     def delete_item(self):
-        # selectionIdxs = self.tbl_widget.selectedIndexes()[0]
-        # print(selectionIdxs)
-        # self.tbl_widget.removeRow(selectionIdxs.row())
-        pass
+        selectionIdxs = self.tbl_widget.selectedIndexes()[0]
+        print(selectionIdxs)
+        self.tbl_widget.removeRow(selectionIdxs.row())
 
     def init_item(self):
         self.ui.le_no.clear()
